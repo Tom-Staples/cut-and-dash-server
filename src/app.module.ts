53bee20e3562286e95b07f872e41512bc64d8538
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-22 09:59:10 +0100

    DB connection setup and environment configured.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      connectionFactory: (connection: Connection) => {
+        if (connection.readyState === 1) {
+          Logger.log('DB Connected');
+        }
+        connection.on('disconnected', () => {
+          Logger.log('DB disconnected');
+        });
+        connection.on('error', (error) => {
+          Logger.error(`DB connection failed. Error: ${error}`);
+        });
+        return connection;
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
