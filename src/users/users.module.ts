26da6a4ece153b71760fa6bbd5bb9d30c844c1b5
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-22 12:28:04 +0100

    Users module config

---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from './users.model';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [],
+})
+export class UsersModule {}
