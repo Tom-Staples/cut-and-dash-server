1c191d05f75f47f43a251df8fe0a53973eed7c70
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-24 10:19:27 +0100

    Auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './local-auth.guard';
+import { AuthService } from './auth.service';
+import { Public } from 'src/decorators/public.decorator';
+import { CreateUserDto } from 'src/users/dto/createUserDto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Public()
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Public()
+  @Post('register')
+  async register(@Body() userData: CreateUserDto) {
+    const userId = await this.authService.register(userData);
+    return userId;
+  }
+}
