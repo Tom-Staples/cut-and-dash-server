c88f16a2d28866eb4049d3a035c203abbbf5df93
Tom Staples <staples.2007@yahoo.co.uk>
2023-05-31 16:05:59 +0100

    implemented separate db for e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module, Logger, ValidationPipe } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Connection } from 'mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -13,20 +13,25 @@ import { PasswordModule } from './password/password.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {
-      connectionFactory: (connection: Connection) => {
-        if (connection.readyState === 1) {
-          Logger.log('DB Connected');
-        }
-        connection.on('disconnected', () => {
-          Logger.log('DB disconnected');
-        });
-        connection.on('error', (error) => {
-          Logger.error(`DB connection failed. Error: ${error}`);
-        });
-        return connection;
+    MongooseModule.forRoot(
+      process.env.NODE_ENV === 'test'
+        ? process.env.MONGODB_TEST_URI
+        : process.env.MONGODB_URI,
+      {
+        connectionFactory: (connection: Connection) => {
+          if (connection.readyState === 1) {
+            Logger.log('DB Connected');
+          }
+          connection.on('disconnected', () => {
+            Logger.log('DB disconnected');
+          });
+          connection.on('error', (error) => {
+            Logger.error(`DB connection failed. Error: ${error}`);
+          });
+          return connection;
+        },
       },
-    }),
+    ),
     UsersModule,
     AuthModule,
     PasswordModule,
